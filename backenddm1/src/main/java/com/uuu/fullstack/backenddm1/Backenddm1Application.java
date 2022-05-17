1cd40a6a482e38b7424a06c356e729eea037a788
dantchan <k82132@gmail.com>
2022-05-17 13:12:04 +0800

    start project spring.io

---
+++ backenddm1/src/main/java/com/uuu/fullstack/backenddm1/Backenddm1Application.java
@@ -0,0 +1,13 @@
+package com.uuu.fullstack.backenddm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backenddm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backenddm1Application.class, args);
+	}
+
+}
---
+++ backenddm1/src/test/java/com/uuu/fullstack/backenddm1/Backenddm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uuu.fullstack.backenddm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backenddm1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
