15356c69947c9d849518bfdaad7e762001e5acf7
dantchan <k82132@gmail.com>
2022-05-18 11:36:55 +0800

    master

---
+++ react_lab/src/Persion/Persion.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Persion = () => {
+  return (
+    <div>
+        <p>Hi I am groot i am the {Math.floor(Math.random()*10)}th generation</p>
+    </div>
+  )
+}
+
+export default Persion
---
+++ react_lab/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hi this is Dashboard</h1>
+        <h1> Hi</h1>
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
