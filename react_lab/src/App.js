d9aa95f3eff30e6654a806f2bfe087c280d6a10b
dantchan <k82132@gmail.com>
2022-05-18 15:18:06 +0800

    onClick error funtion and nomral function

--- react_lab/src/App.js
+++ react_lab/src/App.js
@@ -3,18 +3,37 @@ import React from 'react';
 import Dashboard from './components/Dashboard';
 import Persion from './Persion/Persion';
 import Pet from './Pet/Pet';
-function App() {
-  return (
+class App extends React.Component {
+  state={
+    persons:[{name:"mark",age:44},
+    {name:"frank",age:32},
+    {name:"tim",age:23}],
+    teamMax:10
+  }
+  changeNameHandler = (leaderName) => {
+    console.log("button clicked!!!!!")
+    //this.state.persons[0].name = "Captain America"
+    this.setState({
+      persons: [{ name: leaderName, age: 38 },
+      { name: "Thor", age: 37 },
+      { name: "Iron Man", age: 35 },
+      { name: "Hawk", age: 50 }]
+    })
+  }
+  render() {
+    return (
     <div className="App">
-      <Persion age="45" name="adt"/>
-      <Persion age="42" name="dee"/>
-      <Persion name="dee">Team Leader</Persion>
-      <Pet sprcie="dog" name="doff"/>
+    <button onClick={this.changeNameHandler.bind(this,"Iron Man")}>Change</button>
+    <button onClick={()=>this.changeNameHandler("Iron Man 3")}>Change1</button>
+    <Persion clickCallback={this.changeNameHandler.bind(this,"Bat Man")} name={this.state.persons[0].name} age={this.state.persons[0].age} />
+    <Pet sprcie="dog" name="doff"/>
+    <Persion name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+    <Persion name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
     </div>
     // React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi')
     // ,React.createElement('h1',null,"Hi this is react programming"))
-
-  );
+  )};
 }
 
 export default App;
--- react_lab/src/Persion/Persion.js
+++ react_lab/src/Persion/Persion.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Persion = (props) => {
+    const [stateString, changeString] = useState({publisher:"Marvel Studio"})
+    const showState = () => {
+        console.log(stateString)
+    }
+    const switchVenderHandler = () => {
+        changeString({publisher:"DC COMICS", studio:stateString.studio})
+    }
   return (
     <div>
-        <p> <em style={{color: 'red'}}>I am {props.children}</em> Hi I am {props.name} i am {props.age} age i am the {Math.floor(Math.random()*10)}th generation</p>
+        <h1>{stateString.publisher}</h1>
+        <button onClick={switchVenderHandler}>Change!!</button>
+        <button onClick={showState}>show State</button>
+        <p onClick={props.clickCallback}> <em style={{color: 'red'}}>I am {props.children}</em> Hi I am {props.name} i am {props.age} age i am the {Math.floor(Math.random()*10)}th generation</p>
     </div>
   )
 }
