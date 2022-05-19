40321fa2f2aecf13a2688c0bf90163731a47a81a
dantchan <k82132@gmail.com>
2022-05-19 10:24:50 +0800

    style change

--- react_lab/src/App.js
+++ react_lab/src/App.js
@@ -1,26 +1,35 @@
+//import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Dashboard from './components/Dashboard';
-import Persion from './Persion/Persion';
+import Dashboard from './components/Dashboard.js';
+import Person from './Person/Person';
 import Pet from './Pet/Pet';
 import Banner from './components/Banner';
 class App extends React.Component {
   titleChangeListener = event => {
     this.setState({ title: event.target.value })
   }
-  state = {
-    persons: [{ name: "mark", age: 44 },
-    { name: "frank", age: 32 },
-    { name: "tim", age: 23 }],
+  state = { //2
+    persons: [{ name: "Mark", age: 44 },
+    { name: "James", age: 34 },
+    { name: "Tim", age: 49 },
+    { name: "Kevin", age: 54 }],
     teamMax: 10,
     title: "hello react",
-    showPersons:false
-
+    showPersons: false
   }
   toggleDisplayHandler = () => {
+    console.log(`current state=${this.state.showPersons}, will change`)
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
+    this.setState({ showPersons: !doesShow })
   }
+  deletePerson = index => {
+    const p = [...this.state.persons];
+    p.splice(index,1);
+    this.setState({persons:p});
+  }
+  //changeNameHandler(){
+  //changeNameHandler = function () {
   changeNameHandler = (leaderName) => {
     console.log("button clicked!!!!!")
     //this.state.persons[0].name = "Captain America"
@@ -28,37 +37,48 @@ class App extends React.Component {
       persons: [{ name: leaderName, age: 38 },
       { name: "Thor", age: 37 },
       { name: "Iron Man", age: 35 },
-      { name: "Hawk", age: 50 }]
+      { name: "Hawk", age: 50 }],
+
     })
   }
   render() {
     const style = {
-      backgroundColor: "yellow",
+      backgroundColor: "green",
+      color:'white',
       font: "inherit",
       border: "2px solid red",
       padding: "4px",
       cursor: 'pointer'
     }
-    return (
+    // add this...
+    let persons = null;
+    if (this.state.showPersons === true) {
+      persons = (<div>
+        {this.state.persons.map((p,index) => <Person
+          clickCallback={()=>this.deletePerson(index)}
+          key={p.name}
+          name={p.name}
+          age={p.age} />)}
+      </div>)
+      style.backgroundColor='red'
+      style.color='black'
+    }
+    const classes = [];
+    if (this.state.persons.length<=2) {
+      classes.push('blue');
+    }
+    if (this.state.persons.length<=1) {
+      classes.push('bold')
+    }    return (
       <div className="App">
-        <button style={style} onClick={() => this.toggleDisplayHandler()}>Show/Hide</button>
-        <h1>{this.state.title}</h1>
-        <Banner clickCallback={this.titleChangeListener}></Banner>
-        <button style={style} onClick={this.changeNameHandler.bind(this, "Iron Man")}>Change</button>
-        
-        
-        {this.state.showPersons === true ?
-          <div>
-          <button onClick={() => this.changeNameHandler("Iron Man 3")}>Change1</button>
-        <Persion clickCallback={this.changeNameHandler.bind(this, "Bat Man")} name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Pet sprcie="dog" name="doff" />
-        <Persion name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Persion name={this.state.persons[2].name} age={this.state.persons[2].age} /></div>
-      :null}
-
+      <p className={classes.join(' ')}>{this.state.title}</p>
+      <button style={style} onClick={() => this.toggleDisplayHandler()}>Show/Hide</button><br />
+        <Banner clickCallback={this.titleChangeListener} name={this.state.title}></Banner>
+        <button
+          style={style}
+          onClick={() => this.changeNameHandler("Iron Man")}>Change</button>
+        {persons}
       </div>
-      // React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi')
-      // ,React.createElement('h1',null,"Hi this is react programming"))
     )
   };
 }
